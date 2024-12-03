50b226813c334bb44b07d8534919e250dbc71880
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2024-12-03 13:30:40 +0500

    Change reduce to sum

--- lib/day1.rb
+++ lib/day1.rb
@@ -4,19 +4,13 @@ class Day1
   def part1
     cols = prepared_input.transpose.map(&:sort).transpose
 
-    cols.reduce(0) do |acc, row|
-      acc += (row[1] - row[0]).abs
-      acc
-    end
+    cols.sum { |row| (row[1] - row[0]).abs }
   end
 
   def part2
     cols = prepared_input.transpose
     occurences = cols[1].tally
-    cols[0].reduce(0) do |acc, num|
-      acc += num * (occurences[num] || 0)
-      acc
-    end
+    cols[0].sum { |num| num * (occurences[num] || 0) }
   end
 
   private
